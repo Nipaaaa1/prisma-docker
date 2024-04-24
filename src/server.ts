abe5e6f98a3533da0b010513aa093dc6ae69b683
Nipaaaa1 <Frostone224@gmail.com>
2024-04-24 17:28:15 +0700

    chore: add prettier and format codes

--- src/server.ts
+++ src/server.ts
@@ -1,75 +1,75 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response } from "express";
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-import cors from 'cors'
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/api/users/', async (req: Request, res: Response) => {
-    const getAllUsers = {
-        data: await prisma.user.findMany()
-    }
+app.get("/api/users/", async (req: Request, res: Response) => {
+  const getAllUsers = {
+    data: await prisma.user.findMany(),
+  };
 
-    res.send(getAllUsers)
-})
+  res.send(getAllUsers);
+});
 
-app.get('/api/users/:id', async (req: Request, res: Response) => {
-    const getOneUser = {
-        data: await prisma.user.findFirst({
-            where: {
-                id: req.params.id as string
-            }
-        })
-    }
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+  const getOneUser = {
+    data: await prisma.user.findFirst({
+      where: {
+        id: req.params.id as string,
+      },
+    }),
+  };
 
-    res.send(getOneUser)
-})
+  res.send(getOneUser);
+});
 
-app.post('/api/users/', async (req: Request, res: Response) => {
-    const createUser = {
-        data: await prisma.user.create({
-            data: req.body
-        })
-    }
+app.post("/api/users/", async (req: Request, res: Response) => {
+  const createUser = {
+    data: await prisma.user.create({
+      data: req.body,
+    }),
+  };
 
-    res.send(createUser)
-})
+  res.send(createUser);
+});
 
-app.put('/api/users/:id', async (req: Request, res: Response) => {
-    const updateUser = {
-        data: await prisma.user.update({
-            where: {
-                id: req.params.id as string
-            },
-            data: req.body
-        })
-    }
+app.put("/api/users/:id", async (req: Request, res: Response) => {
+  const updateUser = {
+    data: await prisma.user.update({
+      where: {
+        id: req.params.id as string,
+      },
+      data: req.body,
+    }),
+  };
 
-    res.send(updateUser)
-})
+  res.send(updateUser);
+});
 
-app.delete('/api/users/:id', async (req: Request, res: Response) => {
-    const deleteUser = {
-        data: await prisma.user.delete({
-            where: {
-                id: req.params.id as string
-            },
-        })
-    }
+app.delete("/api/users/:id", async (req: Request, res: Response) => {
+  const deleteUser = {
+    data: await prisma.user.delete({
+      where: {
+        id: req.params.id as string,
+      },
+    }),
+  };
 
-    res.send(deleteUser)
-})
+  res.send(deleteUser);
+});
 
-app.get('/*', (req: Request, res: Response) => {
-    res.send('Please look at the documentation in Github.')
-})
+app.get("/*", (req: Request, res: Response) => {
+  res.send("Please look at the documentation in Github.");
+});
 
 app.listen(3000, () => {
-    console.log("server listening at port 3000")
-})
+  console.log("server listening at port 3000");
+});
