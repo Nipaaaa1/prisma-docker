971d6399c1131f9ac9bdd406dc7a530db0cd70c7
Nipaaaa1 <Frostone224@gmail.com>
2024-04-24 16:29:29 +0700

    fix: add cors

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,14 @@ import express, { Request, Response } from 'express'
 
 import { PrismaClient } from '@prisma/client'
 
+import cors from 'cors'
+
 const app = express()
 
 const prisma = new PrismaClient()
+
+app.use(cors())
+app.use(express.json())
 
 
 app.get('/api/users/', async (req: Request, res: Response) => {
